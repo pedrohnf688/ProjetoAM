e61b6791ba361f58645d4182b1cda173aa12ae41
pedrohnf <pedrohnf2014@gmail.com>
2019-06-08 00:54:09 -0300

    Criando Login

--- src/main/java/com/pedrohnf688/api/controllers/LaudoController.java
+++ src/main/java/com/pedrohnf688/api/controllers/LaudoController.java
@@ -3,12 +3,8 @@ package com.pedrohnf688.api.controllers;
 import java.io.IOException;
 import java.security.NoSuchAlgorithmException;
 import java.text.ParseException;
-import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Optional;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import javax.validation.Valid;
 
@@ -16,17 +12,15 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.pedrohnf688.api.config.Response;
@@ -35,9 +29,8 @@ import com.pedrohnf688.api.repositorio.LaudoRepositorio;
 import com.pedrohnf688.api.service.LaudoService;
 import com.pedrohnf688.api.utils.CsvUtils;
 
-@RestController
-@RequestMapping("/laudo")
-@CrossOrigin(origins = "*")
+@Controller
+@RequestMapping("/")
 public class LaudoController {
 
 	private static final Logger log = LoggerFactory.getLogger(LaudoController.class);
@@ -49,6 +42,12 @@ public class LaudoController {
 	private LaudoRepositorio laudoRepositorio;
 
 	@GetMapping
+	public String a() {
+		return "login.html";
+	}
+
+	
+	@GetMapping(value = "/listar")
 	public List<Laudo> listarLaudos() {
 		List<Laudo> laudos = this.laudoService.listarLaudos();
 		return laudos;
